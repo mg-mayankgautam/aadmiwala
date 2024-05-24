8c9d4cf8ea620d198e92440e96bdcaadd9fdaf46
Mansha <manshapreet02@gmail.com>
2024-05-25 01:47:40 +0530

    sync

---
+++ frontend/src/components/About/AboutTimeline.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+
+const AboutTimeline = () => {
+  return (
+    <div className='AboutTimeline'>
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent >
+          <div className='timelineContent'>
+            <div className='timelineHead'>Our Approach</div>
+            <div className='text'>We believe that effective staffing solutions begin with a deep understanding of your unique needs and challenges. That's why our process starts with a consultative approach, where we take the time to learn about your company culture, operational goals, and staffing requirements. <br /> <br />Through this collaborative effort, we craft bespoke manpower solutions that seamlessly integrate with your existing workforce, ensuring a cohesive and productive team environment.</div>
+          </div>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      {/* <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          10:00 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          12:00 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          9:00 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Repeat</TimelineContent>
+      </TimelineItem> */}
+    </Timeline>
+    </div>
+  );
+}
+
+export default AboutTimeline
