a5fb7ef46803d6abffd6c0460164ff40210e5975
Mansha <manshapreet02@gmail.com>
2024-05-28 03:20:15 +0530

    companies on homepage + regex msgs and next btn

--- frontend/src/components/Home/HomeMain.jsx
+++ frontend/src/components/Home/HomeMain.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 import bg from '../../assets/OBJECTS.png'
 import SearchIcon from '@mui/icons-material/Search';
 
 const HomeMain = () => {
+
+
+    const [companies, setCompanies] =useState([])
+
+    useEffect(()=>{
+
+        const getCompanies= async()=>{
+          try{
+            const data = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/getallcompanies`);
+            console.log(data);
+            const array = data.data
+            setCompanies(array);
+            // setpublicLists(publiclists);
+          }
+          catch(err){console.log(err);}
+        }
+    
+        getCompanies()
+      
+      },[]);
+
+
   return (
     <>
     <div className='HomeMain'>
@@ -67,39 +90,22 @@ const HomeMain = () => {
         
         <div className='FC_container'>
 
-            <div className='FC_box'>
-                <div className='new'>New</div>
-                <div className='FC_name'>ABC Recruiting Firm</div>
-                <div>stars</div>
-                <div>XYZ Office, <br/> Delhi, India</div>
-                <div className='FC_facilities'>Provide Blue Collars & Facilities Provider</div>
-                <div className='FC_desc'>
-                Company Description: <li>Brief Description: XYZ Corporation is seeking 15 Facilities Providers to join our team at the XYZ Office. The Facilities Providers will be responsible for maintaining the cleanliness and functionality of our office facilities.</li> 
-                </div>
-                <div className='FC_date'>Posted 2 Days ago</div>
-            </div>
-            <div className='FC_box'>
-                <div className='new'>New</div>
-                <div className='FC_name'>ABC Recruiting Firm</div>
-                <div>stars</div>
-                <div>XYZ Office, <br/> Delhi, India</div>
-                <div className='FC_facilities'>Provide Blue Collars & Facilities Provider</div>
-                <div className='FC_desc'>
-                Company Description: <li>Brief Description: XYZ Corporation is seeking 15 Facilities Providers to join our team at the XYZ Office. The Facilities Providers will be responsible for maintaining the cleanliness and functionality of our office facilities.</li> 
-                </div>
-                <div className='FC_date'>Posted 2 Days ago</div>
-            </div>
-            <div className='FC_box'>
-                <div className='new'>New</div>
-                <div className='FC_name'>ABC Recruiting Firm</div>
-                <div>stars</div>
-                <div>XYZ Office, <br/> Delhi, India</div>
-                <div className='FC_facilities'>Provide Blue Collars & Facilities Provider</div>
-                <div className='FC_desc'>
-                Company Description: <li>Brief Description: XYZ Corporation is seeking 15 Facilities Providers to join our team at the XYZ Office. The Facilities Providers will be responsible for maintaining the cleanliness and functionality of our office facilities.</li> 
-                </div>
-                <div className='FC_date'>Posted 2 Days ago</div>
-            </div>
+            {companies&& companies.map(company=>
+
+                        <div className='FC_box'>
+                            <div className='new'>New</div>
+                            <div className='FC_name'>{company.companyName}</div>
+                            <div>stars</div>
+                            <div>{company.city}<br/>{company.state}, {company.country}</div>
+                            <div className='FC_facilities'>Provides {company.serviceType.map(service=><span>{service}, </span>)}</div>
+                            <div className='FC_desc'>
+                            Company Description: <li>{company.agencyBriefing}</li> 
+                            </div>
+                            <div className='FC_date'>Posted 2 Days ago</div>
+                        </div>
+
+                )}
+            
            
         </div>
 
