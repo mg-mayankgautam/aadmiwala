13ac1f679d42d650b4854917e10e280ced16de09
Mansha <manshapreet02@gmail.com>
2024-07-15 19:36:25 +0530

    cors error fix

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,8 @@ app.use((req, res, next)=>{
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
-    )
+    );
+    next();
 })
 
 const mongoose = require('mongoose');
