65621f4b3e84159377609f4e94162c738ea4f45f
Mansha <manshapreet02@gmail.com>
2024-07-15 19:34:46 +0530

    cors error

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,20 @@ app.use(cors(
     }
 ))
 
+app.use((req, res, next)=>{
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://covendx.com/"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    )
+})
 
 const mongoose = require('mongoose');
 // const { mongoConnect } = require('./database/database.js');
