008374caeebe6e9b67e6a983e2d0508c317d6fc0
Mansha <manshapreet02@gmail.com>
2024-07-21 18:19:56 +0530

    4steps.png

--- frontend/src/components/Home/Benefits.jsx
+++ frontend/src/components/Home/Benefits.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
-import steps from '../../assets/steps.png'
+import Line from '../Line/Line'
+import steps from '../../assets/4steps.png'
+import Svg from './Svg'
+
 
 const Benefits = () => {
   return (
     <div className='Benefits'>
-        <div className='benefitsHead'>
-          Upgrade your workforce in 4 easy steps:
+
+        <div className='stepsContainer'>
+          <img src={steps}/>
         </div>
 
-        <div>
+        {/* <div className='benefitsHead'>
+          Upgrade your workforce in 4 easy steps:
+        </div> */}
+
+        {/* <div>
           <img src={steps} className='stepsImg'/>
-        </div>
-        <div className='benefitsContainer'>
+        </div> */}
+        {/* <div className='benefitsContainer'>
             <div className='benefitCard'>
                 <div className='BC_text_bold'>STEP</div>
                 <div className='BC_num'>01</div>
                 <div className='BC_text'>Customize Your Needs</div>
             </div>
+
+          <Line/>
+
             <div className='benefitCard'>
                 <div className='BC_text_bold'>STEP</div>
                 <div className='BC_num'>02</div>
                 <div className='BC_text'>Connect with Service providers</div>
-            </div>
-            <div className='benefitCard'>
+            </div> */}
+            {/* <div className='benefitCard'>
                 <div className='BC_text_bold'>STEP</div>
                 <div className='BC_num'>03</div>
                 <div className='BC_text'>Confirm Your Choices</div>
@@ -31,8 +42,8 @@ const Benefits = () => {
                 <div className='BC_text_bold'>STEP</div>
                 <div className='BC_num'>04</div>
                 <div className='BC_text'>Empower Your Team</div>
-            </div>
-        </div>
+            </div> */}
+        {/* </div> */}
 
 
         {/* <div className='benefitsText'>
@@ -40,7 +51,7 @@ const Benefits = () => {
           <div className='text'>
             Seamlessly tap into our pool of best service providers, optimize your workforce management, and minimize administrative burdens. Our mission is to deliver high-quality staffing solutions efficiently, empowering you to focus on propelling your business towards growth and success.
           </div>
-        </div> */}
+        </div> */}    
     </div>
   )
 }
---
+++ frontend/src/components/Line/Line.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Line.css'
+
+const Line = () => {
+  return (
+    <div className='Line'>
+            <div className='Line_line'></div>
+            <div className='Line_circle'></div>
+        </div>
+  )
+}
+
+export default Line
