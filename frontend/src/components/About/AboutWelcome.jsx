ec86510c915db939d80f3f4ff9302fb71b06026f
John Doe <johndoe@email.com>
2024-07-27 20:02:26 +0530

    new changes

--- frontend/src/components/About/AboutWelcome.jsx
+++ frontend/src/components/About/AboutWelcome.jsx
@@ -9,9 +9,9 @@ const AboutWelcome = ({about_ref}) => {
         <div>
             <div className='aboutSubHead'>Welcome to</div>
             <div className='aboutHead'>Covendx</div>
-            <div className='subHead'>your trusted partner for tailored staffing <br /> solutions that meets seamless integration. </div>
+            <div className='subHead'>your trusted partner for tailored vendors <br /> solutions that meets seamless integration. </div>
         </div>
-        <div className='text'>Our platform simplifies the process of connecting with the right agencies, allowing clients to focus on their core business while we handle the intricacies of staffing.
+        <div className='text'>Our platform simplifies the process of connecting with the right agencies, allowing clients to focus on their core business while we handle the intricacies of vendors.
         </div>
         <div>
             <button className='aboutBtn' 
