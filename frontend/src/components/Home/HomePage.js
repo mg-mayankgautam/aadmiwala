d77aec86ac1583302388eb4b5a0ad1af46ea3f59
Mansha <manshapreet02@gmail.com>
2024-09-10 17:25:59 +0530

    scroll to top home pg

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import Nav from '../Nav/Nav'
 import './Home.css'
 import HomeMain from './HomeMain/HomeMain'
