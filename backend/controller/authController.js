b99d8156eb13ea2647d53cda3b0ebf8b69ff8354
John Doe <johndoe@email.com>
2024-09-20 15:09:00 +0530

    res.send done

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -303,7 +303,7 @@ module.exports.addRecruitingCompany= async (req,res)=>{
 
 module.exports.editBlogData=async(req,res)=>{
 S3Data=!S3Data;
-res.send('done')
+res.send('done',S3Data)
 }
 
 module.exports.getCompanies =async (req, res) =>{
